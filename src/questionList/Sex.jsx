23c5765b9d75eea7c6a5f4cb15e2382c633b724d
lykuuu <46769549+lykuuu@users.noreply.github.com>
2020-04-18 19:51:00 +0800

    Update Sex.jsx

--- src/questionList/Sex.jsx
+++ src/questionList/Sex.jsx
@@ -12,8 +12,8 @@ class Sex extends React.Component {
                 </Form.Group>
                 <Form.Group as={Col} md={9}>
                     <div className="mb-3">
-                        <Form.Check inline label="Female" type={'radio'} name="sex" value={"1"} onChange={this.props.handleChange} />
-                        <Form.Check inline label="Male" type={'radio'} name="sex" value={"2"} onChange={this.props.handleChange} />
+                        <Form.Check inline label="Female" type={'radio'} name="sex" value={"F"} onChange={this.props.handleChange} />
+                        <Form.Check inline label="Male" type={'radio'} name="sex" value={"M"} onChange={this.props.handleChange} />
                     </div>
                 </Form.Group>
             </Form.Row>
