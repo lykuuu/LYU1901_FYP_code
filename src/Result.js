1bee56b1738e56b201ebb3adccf3aad94f38850c
Yuk Lung LUI <1155092435@link.cuhk.edu.hk>
2020-05-03 22:36:54 +0800

    Result update

--- src/Result.js
+++ src/Result.js
@@ -17,7 +17,7 @@ let Information = [
     { "priLangTime": [0, 0, 0, 0, 0] },
     { "suppLang": [0, 0, 0, 0, 0] },
     { "hours": [0, 0, 0, 0, 0, 0] },
-    { "expGrade:": [0, 0, 0, 0, 0, 0] },
+    { "expGrade": [0, 0, 0, 0, 0, 0] },
 ];
 
 let question = [
@@ -53,7 +53,7 @@ const InformationH = [
     ["My faculty", "ART", "BAS", "EDU", "ERG", "LAW", "MED", "SCI", "SSC", "OTHER"],
     ["Level", "Undergraduate", "Postgraduate", "Other"],
     ["Year of study", "Year 1", "Year 2", "Year 3", "Year 4", "Year 5", "Year >= 6"],
-    ["This course is", "Major Required", "Major Elective", "Minor", "Elective", "U Core"],
+    ["This course is", "Major Required", "Major Elective", "Minor", "Elective", "U Core", "N/A"],
     ["Sex", "Female", "Male"],
     ["Primary spoken language used in class", "English", "Cantonese", "Putonghua", "Others"],
     ["Fraction of class time the primary language was used", "51-60%", "61-70%", "71-80%", "81-90%", "91-100%"],
@@ -70,7 +70,8 @@ class Result extends React.Component {
         this.state = {
             privateKey: "XXX",
             searchSuccess: false,
-            neverSearch: true
+            neverSearch: true,
+            updated: false
             /* ,
              */
         }
@@ -85,21 +86,24 @@ class Result extends React.Component {
             courseID = prompt("Please enter course ID.");
         }
         //replace with real search code
-        if (searchID !== "") {
+        if (searchID !== "" && (courseID !== null || courseID !== "")) {
             this.setState({
                 privateKey: searchID
             });
-
+            CourseID = courseID;
             fetch(`http://3.113.9.168:8080/api/course/${courseID}`).then(response => {
                 if (response.status !== 200) {
                     //TODO: change it to bad response reaction
-                    alert("Bad")
+                    alert("Please check your course id")
+                    this.setState({
+                        updated: false
+                    })
                 }
                 else {
                     response.json().then(json => {
-
+                        alert("loading")
                         let { result, courseInfo } = json
-
+                        
                         for (const key in result) {
                             result[key] = result[key].map((v, i) => {
                                 return decrypt(v, courseInfo.p, courseInfo.g, this.state.privateKey)
@@ -107,17 +111,77 @@ class Result extends React.Component {
                         }
 
                         //TOD: link result to display,result is the already decrypted format
+                        Information[0].faculty = result.faculty;
+                        Information[1].level = result.level;
+                        Information[2].studyYear = result.studyYear;
+                        Information[3].courseProperty = result.courseProperty;
+                        Information[4].sex = result.sex;
+                        Information[5].priLang = result.priLang;
+                        Information[6].priLangTime = result.priLangTime;
+                        Information[7].suppLang = result.suppLang;
+                        Information[8].hours = result.hours;
+                        Information[9].expGrade = result.expGrade;
+
+                        var Rcounter = 0;
+                        var tmp = [];
+                        var stage = 0;
+                        for (const key in result) {
+                            if (Rcounter > 9) {
+                                tmp.push(result[key]);
+                            }
+                            Rcounter += 1;
+                        }
+                        for (var i = 0; i < 20; i++) {
+                            if (i === 12 || i === 15) {
+                                stage += 1;
+                                if (i === 12) {
+                                    Complain1[0].M = tmp[i][0];
+                                    Complain1[0].L = tmp[i][1];
+                                }
+                                if (i === 15) {
+                                    Complain2[0].D = tmp[i][0];
+                                    Complain2[0].S = tmp[i][1];
+                                }
+                                continue;
+                            }
+                            question[i - stage].StD = tmp[i][0];
+                            question[i - stage].D = tmp[i][1];
+                            question[i - stage].SlD = tmp[i][2];
+                            question[i - stage].SlA = tmp[i][3];
+                            question[i - stage].A = tmp[i][4];
+                            question[i - stage].StA = tmp[i][5];
+                            question[i - stage].NA = tmp[i][6];
+                            var total = parseInt(tmp[i][0]) + parseInt(tmp[i][1]) + parseInt(tmp[i][2]) + parseInt(tmp[i][3]) + parseInt(tmp[i][4]) + parseInt(tmp[i][5]) + parseInt(tmp[i][6]);
+                            var Average = (1 * parseInt(tmp[i][0]) + 2 * parseInt(tmp[i][1]) + 3 * parseInt(tmp[i][2]) + 4 * parseInt(tmp[i][3]) + 5 * parseInt(tmp[i][4]) + 6 * parseInt(tmp[i][5]) + 0 * parseInt(tmp[i][6])) / total;
+                            if (Math.floor(Average) === 0)
+                                question[i - stage].Average = "N/A";
+                            if (Math.floor(Average) === 1)
+                                question[i - stage].Average = "Strongly Disagree";
+                            if (Math.floor(Average) === 2)
+                                question[i - stage].Average = "Disagree"
+                            if (Math.floor(Average) === 3)
+                                question[i - stage].Average = "Slightly Disagree"
+                            if (Math.floor(Average) === 4)
+                                question[i - stage].Average = "Slightly Agree"
+                            if (Math.floor(Average) === 5)
+                                question[i - stage].Average = "Agree"
+                            if (Math.floor(Average) === 6)
+                                question[i - stage].Average = "Strongly Agree"
+
+                        }
+                        console.log(tmp);
+                        console.log(question)
                         console.log(result)
+                        this.setState({
+                            updated: true,
+                            searchSuccess:found
+                        })
 
                     })
                 }
 
             })
 
-
-            this.setState({
-                searchSuccess: found
-            });
         } else {
             this.setState({
                 searchSuccess: false
@@ -219,7 +283,7 @@ class Result extends React.Component {
                 </InputGroup>
 
                 {
-                    this.state.searchSuccess && <div>
+                    this.state.searchSuccess && this.state.updated && <div>
                         <h3 className="pt-5">Results of {CourseID}</h3>
 
                         <h4 className="pt-5 pb-3 text-left"><b>Student Information</b></h4>
@@ -271,11 +335,8 @@ class Result extends React.Component {
                     </div>
                 }
                 {
-                    !this.state.searchSuccess && !this.state.neverSearch && <div>
+                    !this.state.searchSuccess && !this.state.neverSearch && !this.state.updated && <div>
                         <h3 className="pt-2">Results of {CourseID}</h3>
-                        <div className="pt-4 text-center">
-                            No related results
-                        </div>
                     </div>
                 }
 
